044f84524a4381b2eb67506d462d4be0cdcd4f1e
atlaschuen@gmail.com <junrongquan@junrongdeMacBook-Air.local>
2019-06-23 00:28:10 +0800

    a draft for front-end logic

--- v1/dapp/js/create.js
+++ v1/dapp/js/create.js
@@ -2,9 +2,20 @@ var abi = '';
 var bin = '';
 
 $(function () {
+    $('.image-upload-wrap').bind('dragover', function () {
+             $('.image-upload-wrap').addClass('image-dropping');
+    });
+    $('.image-upload-wrap').bind('dragleave', function () {
+             $('.image-upload-wrap').removeClass('image-dropping');
+    });
+
     getAbi();
     getBin();
 });
+
+var _default = {
+    escrow: 600,    //600s, just for easy to develop now
+}
 
 var create = function () {
     web3.cmt.getAccounts(function (e, address) {
@@ -14,8 +25,9 @@ var create = function () {
             var userAddress = address.toString();
             var title = $('#title').val();
             var desc = $('#desc').val();
-            var imageUrl = $('#imageUrl').val();
-            var escrow = $('#escrow').val();
+            // var imageUrl = $('#imageUrl').val();
+            var imageUrl = $('#uploaded').attr("src");
+            var escrow = _default.escrow;
             var tags = $('#tags').val();
             var categories = $('#categories').val();
             var crc20 = "0xce9a6ec5f153b87ad0f05915c85dbd3a0f6ed99a";
@@ -80,4 +92,69 @@ var getBin = function () {
             bin = JSON.parse(data);
         }
     });
+}
+
+function removeUpload() {
+  $('.file-upload-input').replaceWith($('.file-upload-input').clone());
+  $('.file-upload-content').hide();
+  $(".uploading-text").addClass("d-none");
+
+  $('.image-upload-wrap').show();
+}
+
+$('#img-form').ajaxForm({
+    beforeSubmit: function(){
+        var imgname = $("#selected-img").val()
+        var ext = imgname.substr( (imgname.lastIndexOf('.') +1) );
+        var imgsize = $('#selected-img')[0].files[0].size / 1024 / 1024; // in MB
+        console.log(ext, imgsize)
+        if (ext=='jpg' || ext=='jpeg' || ext=='png' || ext=='gif' || ext=='PNG' || ext=='JPG' || ext=='JPEG'){
+            if(imgsize <= 3){
+                $(".uploading-text").removeClass("d-none");
+                $(".drag-text").addClass("d-none");
+
+                return true;
+            }
+        }
+        
+        $("#local-input").removeClass("d-none");
+        tip.error(lgb["fail_to_upload"] || "Fail to upload. Please check your size and extension.");
+        return false;
+    },
+    success: function(data) {
+        url = data['secure_url']
+
+        $(".uploading-text").addClass("d-none");
+        $(".drag-text").removeClass("d-none");
+
+        $('.image-upload-wrap').hide();
+        $("#uploaded").attr("src", url)
+        $('.file-upload-content').show();
+        $('.image-title').html($("#selected-img").val());
+
+        $("#img").val(url)
+    }
+}); 
+
+
+function uploadPic(){
+    console.log("upload")
+    $(".image-upload-wrap").css("background-color","#999999")
+    $(".image-upload-wrap").css('border','1px dashed #ffffff') 
+    if($("#selected-img").val()){
+           console.log("start")
+          $("#submit").click()
+    }
+}
+
+function changeBoxCSS(){
+    console.log("changeBoxCSS")
+ $(".image-upload-wrap").css('background-color','#999999')
+ $(".image-upload-wrap").css('border','1px dashed #ffffff') 
+ $(".image-upload-wrap").css('transition','.5s')
+  setTimeout(function() {
+    $(".image-upload-wrap").css('background-color','transparent') 
+    $(".image-upload-wrap").css('border','1px dashed #999999') 
+
+  }, 500);
 }
--- v1/dapp/js/listing.js
+++ v1/dapp/js/listing.js
@@ -15,6 +15,7 @@ var bin = '';
 var contract = '';
 var instance = '';  // contract instance
 var tokens = {};
+var imagesCount = '';
 
 $(function () {
     getAbi();
@@ -28,14 +29,30 @@ $(function () {
     }, 50);
 });
 
+var initBuyer = (buyerPanel, buyer) => {
+    // buyerPanel.removeClass("d-none").find("input,textarea,select").attr("disabled", true)
+    // buyerPanel.find("#name-field").val(buyer.name)
+    // buyerPanel.find("#mesg-field").val(buyer.msg)
+    // var cc = buyer.contact.split(":");
+    // buyerPanel.find('#contact-app-field').val(cc[0].trim());
+    // buyerPanel.find('#contact-id-field').val(cc[1].trim());
+
+    // buyerPanel.find('h3').text("Buyer Infomation")
+    // buyerPanel.find('label').hide()
+    // buyerPanel.find('#buy-submit').hide()
+
+    //TODO price setting
+    buyerInfoStr = "addr: " + buyer.addr + "<br>" + 
+                   "name: " + buyer.name + "<br>" +
+                   "remark: " + buyer.msg  + "<br>" +
+                   "payment date: " + new Date(buyer.boughtTime*1000) + "<br>" +
+                   "contact: " + buyer.contact + "<br>"
+    $(".buyer-info").append(buyerInfoStr)
+
+}
+
 var getInfo = function () {
     $('#info-panel').css("display", "block");
-    $('#buy-panel').css("display", "none");
-    $('#dispute-panel').css("display", "none");
-    $('#resolve-panel').css("display", "none");
-    $('#escrowed-panel').css("display", "none");
-    $('#sold-panel').css("display", "none");
-    $('#seller-panel').css("display", "none");
 
     web3.cmt.getAccounts(function (e, address) {
         if (e) {
@@ -53,44 +70,105 @@ var getInfo = function () {
                     var status = r[0];
                     $('#title-div').text(r[1]);
                     $('#desc-panel').text(r[2]);
-                    $('#escrow').text(r[3]);
-                    var imagesCount = r[4];
+                    var escrowDuration = r[3];
+                    $('#escrow').text(escrowDuration);
+                    imagesCount = r[4];
                     var pricesCount = r[5];
                     ownerAddress = r[6].toString();
                     buyerAddress = r[7].toString();
 
+
                     $('#creator').text(ownerAddress);
-
-                    if (status == 1) {
-                        $('#buy-panel').css("display", "block");
-                    } else if (status == 2) {
-                        if (buyerAddress != userAddress) {
-                            $('#escrowed-panel').css("display", "block");
+                    console.log("current status:", status)
+                    if (status == 0) {//unlisting
+                        if(userAddress == ownerAddress){
+                            //1. edit
+                            //2. relist
+                            $(".d-seller.d-unlisting").removeClass("d-none")
+                        }else{
+                            $(".d-other.d-unlisting").removeClass("d-none")
                         }
+                    }else if (status == 1) {//open to buy
+                        if(userAddress == ownerAddress){
+                            //1. edit
+                            //2. unlist
+                            //3. buy?
+                            $('.d-seller.d-open').removeClass("d-none")
+                        }else{
+                            $('.d-other.d-open').removeClass("d-none")
+                        }
+                    } else if (status == 2) {//buyer in escrow
+                        if (buyerAddress != userAddress && userAddress != ownerAddress) {
+                            $('.d-other.d-escrow').removeClass('d-none');
+                        }
+                        //other cases dealt in "instance.buyerInfo"
                     } else if (status == 3) {
-                        $('#sold-panel').css("display", "block");
-                    }
-
-                    if (userAddress == ownerAddress) {
-                        if (status == 1 || status == 2) {
-                            $('#seller-panel').css("display", "block");
+                        if (userAddress != buyerAddress && userAddress != ownerAddress) {
+                            $(".d-other.d-closed.d-closed-sold").removeClass('d-none')
                         }
+                        //other cases dealt in "instance.buyerInfo"
                     }
 
                     instance.buyerInfo (function (be, br) {
                         if (be) {
                             console.log(be);
                         } else {
+                            var buyer = {
+                                addr: br[0],
+                                boughtTime: br[1],
+                                name: br[5],
+                                contact: br[6],
+                                msg: br[7],
+                            }
                             var dispute = br[3];
-                            // TODO: Fill in seller panel
-                            if (buyerAddress == userAddress && status == 2) {
-                                // TODO: allow user to update info
-                                if (dispute) {
-                                    $('#resolve-panel').css("display", "block");
-                                } else {
-                                    $('#dispute-panel').css("display", "block");
+                            //check if time is up to receive
+                            var expiredTime = parseInt(br[1]) + parseInt(escrowDuration)
+                            if (expiredTime.toString().length == 10) {
+                                var expiredDate = new Date(expiredTime * 1000);
+                            } else if (expiredTime.toString().length == 13) {
+                                var expiredDate = new Date(expiredTime);
+                            }
+                            var currentDate = new Date()
+                            console.log(expiredDate)
+                            currentDate > expiredDate ? escrowDone = true : escrowDone = false
+                            if (status == 2) {
+                                //init the buyer info panel
+                                if (userAddress == ownerAddress) {
+                                    initBuyer($("#buy-panel"), buyer)
+                                    if(dispute){
+                                        $(".d-seller.d-escrow.d-escrow-disputing").removeClass('d-none')                                    
+                                    }else{
+                                        if(escrowDone){
+                                            $(".d-seller.d-escrow.d-escrow-done").removeClass('d-none')
+                                        }else{
+                                            $(".d-seller.d-escrow.d-escrow-going").removeClass('d-none')
+                                        }     
+                                    }
+                                   
                                 }
-                            }            
+                                if (userAddress == buyerAddress) {
+                                    initBuyer($("#buy-panel"), buyer)
+                                    // TODO: allow user to update info(when?)
+                                    if (dispute) {
+                                        $(".d-buyer.d-escrow.d-escrow-disputing").removeClass('d-none')                                    
+                                    } else {
+                                        if(escrowDone){
+                                            $(".d-buyer.d-escrow.d-escrow-done").removeClass('d-none')
+                                        }else{
+                                            $(".d-buyer.d-escrow.d-escrow-going").removeClass('d-none')
+                                        }
+                                    }
+                                }
+                            }
+                            
+                            // soldTime = br[2]; //sold time (if the state is closed because of "resolve" , the sold time should be 0)
+                            // soldTime == 0 ? resolved = true : resolved = false
+                            if (userAddress == ownerAddress && status == 3) {
+                                $(".d-seller.d-closed.d-closed-sold").removeClass("d-none")
+                            }   
+                            if (userAddress == buyerAddress && status == 3) {
+                                $(".d-buyer.d-closed.d-closed-sold").removeClass("d-none")
+                            }         
                         }
                     });
 
@@ -100,7 +178,7 @@ var getInfo = function () {
                             if (e_img) {
                                 console.log(e_img);
                             } else {
-                                $('#image-img').html('<img src="' + r_img + '" class="img-fluid img-thumbnail">');
+                                $('#image-img').append('<img src="' + r_img + '" class="img-fluid img-thumbnail">');
                             }
                         }); // getImage
                     }
@@ -115,7 +193,6 @@ var getInfo = function () {
                                 var token_crc20 = r_price[0].toString();
                                 tokens[token_crc20] = r_price[1];
                                 var token_amount = "";
-
                                 if (token_crc20 == "0x0000000000000000000000000000000000000000") {
                                     token_name = "CMT";
                                     token_amount = web3.fromWei(tokens[token_crc20]);
@@ -124,11 +201,20 @@ var getInfo = function () {
                                     token_amount = (parseInt(tokens[token_crc20]) / 100).toString();
                                 }
                                 $('#prices-select').append($("<option></option>").attr("value",token_crc20).text(token_amount + " " + token_name));  
+                                $("#price-desc").append("<div>" + token_amount + " " + token_name + "</div>")
                             }
                         }); // getPrice
                     }
                 }
             }); // info
+            instance.getSellerMesg(function(e,r){
+                console.log(r!="")
+                if(r != "" && (userAddress == ownerAddress || userAddress == buyerAddress)){
+                    $('.seller-mesg').text(r)
+                }else{
+                    $('#seller-mesg-text').detach()
+                }
+            });//sellerMesg
         }
     }); // getAccounts
 }
@@ -291,11 +377,57 @@ var dispute = function () {
     });
 }
 
+var unlist = function () {
+    instance.pause ({
+        gas: '400000',
+        gasPrice: 0
+    }, function (e, result) {
+        if (e) {
+            console.log(e);
+        } else {
+            setTimeout(function () {
+                window.location.reload(true);
+            }, 20 * 1000);
+        }
+    });
+}
+
+var relist = function() {
+    instance.resume ({
+        gas: '400000',
+        gasPrice: 0
+    }, function (e, result) {
+        if (e) {
+            console.log(e);
+        } else {
+            setTimeout(function () {
+                window.location.reload(true);
+            }, 20 * 1000);
+        }
+    });
+}
+
+var setSellerMesg = function (mesg) {
+    instance.setSellerMesg ( mesg, {
+        gas: '400000',
+        gasPrice: 0
+    }, function (e, result) {
+        if (e) {
+            console.log(e);
+        } else {
+            setTimeout(function () {
+                window.location.reload(true);
+            }, 20 * 1000);
+        }
+    });   
+}
+
 var addImage = function () {
-    window.location = "addImage.html?contract=" + contract_address;
+    window.location = "addImage.html?contract=" + contract_address + "&imagesCount=" + imagesCount;
 }
 
 var setPrice = function () {
     window.location = "setPrice.html?contract=" + contract_address;
 }
+
 
