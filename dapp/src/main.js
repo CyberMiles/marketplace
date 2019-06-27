c6a6b9b78073a72e6c65a99613eb5ceb30da2cdd
wangshishuo <wangshishuo@gmail.com>
2019-06-27 18:08:54 +0800

    add contracts

--- dapp/src/main.js
+++ dapp/src/main.js
@@ -1,8 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import Contracts from "./contracts";
 
 Vue.config.productionTip = false;
+
+console.log(Contracts);
 
 new Vue({
   router,
