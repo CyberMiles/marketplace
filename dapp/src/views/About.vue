93384fbc031c4d692baf8aaea502e0d53b3e89e0
atlasquan <atlasquan98@gmail.com>
2019-08-14 19:09:47 +0800

    fix typo

--- dapp/src/views/About.vue
+++ dapp/src/views/About.vue
@@ -17,7 +17,7 @@
       </li>
       <li>
         The {{ USDunit }}s are frozen and secured in the smart contract and
-        cannot be accessed by anymore.
+        cannot be accessed by any one.
       </li>
     </ul>
   </div>
